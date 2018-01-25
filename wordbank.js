e879ea53049269a7242f58db5fde23bcbd39c44d
Arnold R Zimmerman Jr <arnoldrz2@gmail.com>
2018-01-25 17:12:17 -0600

    created wordbank, word, and letter js files

---
+++ wordbank.js
@@ -0,0 +1,2 @@
+//An array of words that will potentially be used when playing the hangman game.
+var wordbank = ["megadeth", "slayer", "metallica", "iron\xa0maiden", "judas\xa0priest", "slipknot", "children\xa0of\xa0bodom", "tool", "alice\xa0in\xa0chains"];
