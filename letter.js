145ccac9b8814bc98e93d7da21260218ef8e24a7
arnold <arnoldrz2@gmail.com>
2018-01-30 00:33:38 -0600

    package update, tweaks to wordbank, and began letter

--- letter.js
+++ letter.js
@@ -1,2 +1,15 @@
-//Used for each letter in the current word. Each letter object should either display an underlying character, or a blank placeholder (such as an underscore),
-//depending on whether or not the user has guessed the letter. This should contain letter specific logic and data.
+//Used for each letter in the current word. Each letter object should either
+//display an underlying character, or a blank placeholder (such as an underscore),
+//depending on whether or not the user has guessed the letter.
+//This should contain letter specific logic and data.
+var Letter = function (ltr) {
+  this.letter = ltr;
+  this.appear = false;
+
+  this.letterRender = function() {
+    if(this.letter == ' '){
+      this.appear = true;
+      return '  ';
+    }
+  }
+}
--- wordbank.js
+++ wordbank.js
@@ -1,2 +1,5 @@
 //An array of words that will potentially be used when playing the hangman game.
-var wordbank = ["megadeth", "slayer", "metallica", "iron\xa0maiden", "judas\xa0priest", "slipknot", "children\xa0of\xa0bodom", "tool", "alice\xa0in\xa0chains"];
+exports.newWord = {
+  wordlist: [ "megadeth", "slayer", "metallica", "iron maiden", "judas priest",
+              "slipknot", "children of bodom", "tool", "lamb of god" ]
+}
