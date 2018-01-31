598aa668b538a300936759d23b97924de6b856a9
Arnold R Zimmerman Jr <arnoldrz2@gmail.com>
2018-01-31 12:23:34 -0600

    added final chunk of syntax. fine tuning still needed

--- hangman.js
+++ hangman.js
@@ -63,6 +63,53 @@ var hangman = {
         }
       }]).then(function(ltr) {
         var letterReturned = (ltr.chosenLtr);
-      })
+        var guessedAlready = false;
+          for(var i = 0; i<this.guessedLetters[i]){
+            if(letterReturned === this.guessedLetter[i]){
+              guessedAlready =true;
+            }
+          }
+          if(guessedAlready === false){
+            this.guessedLetter.push(letterReturned);
+
+            var found = that.currentWord.letterCheck(letterReturned);
+
+            if(found === 0){
+              console.log("Wrong!");
+              this.guessesRemaining--;
+              this.display++;
+              console.log("Guesses remain: " + this.guessesRemaining);
+              console.log(this.display); //check this later
+
+              console.log("\n***************");
+              console.log(this.currentWord.wordRender());
+              console.log("\n***************");
+
+              console.log("Letters guessed: " + this.guessedLetters);
+            }else{
+              console.log("Woohoo! You guessed correctly!");
+
+                if(this.currentWord.wordCheck() === true){
+                  console.log(this.currentWord.wordRender());
+                  console.log("Congrats! You won!");
+                } else {
+                  console.log("Guesses remianing: " + this.guessesRemaining);
+                  console.log(this.currentWord.wordRender());
+                  console.log("\n***************");
+                  console.log("Letters guessed: " + this.guessedLetters);
+                }
+            }
+            if(this.guessesRemaining > 0 && this.currentWord.wordFound === false) {
+              this.userPrompts();
+            }else if(this.guessesRemaining === 0){
+              console.log("Game over!");
+              console.log("The word you were guessing was: " + this.currentWord.word);
+            }
+          } else {
+              console.log("You've guessed that letter already. Try again.");
+              this.userPrompts();
+          }
+      });
     }
 }
+hangman.startGame();
